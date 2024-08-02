7a78638e347405c19742eeb88364a47e0563026f
Aramen Meza <arammendoza.97@gmail.com>
2024-08-02 12:58:43 -0600

    prueba

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,15 @@
-
 import Footer from "@/Containers/Navs/Footer";
 import Header from "@/Containers/Navs/Header";
 import Users from "@/Containers/User";
 import Register from "@/Containers/Register";
 
 export default function Home() {
-
-  const res = await fetch('http://localhost:8080/api/v1/posts')
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
- 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
- console.log(res)
-  return res.js
-
-
   return (
     <>
-    <Header/>
+      <Header />
       <main className="flex  flex-col py-24 px-[18%]">
-        <Users/>
-        <Register/>
+        <Users />
+        <Register />
       </main>
       <Footer />
     </>
